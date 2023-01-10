6fe3529905486564e853686a740f6329fa45420e
Galovach Andrey <a.galovach@yandex.ru>
2023-01-10 20:19:56 +0300

    Переписал RabbitService

--- worker/src/service/consumer.py
+++ worker/src/service/consumer.py
@@ -1,39 +1,19 @@
 import json
 
-import pika
-from app.src.core.config import settings
+from aio_pika.abc import AbstractRobustConnection
 
 
-class RabbitBase:
-    def __init__(self, queue: str) -> None:
-        self.conn_params = pika.ConnectionParameters(settings.rabbit.HOST, settings.rabbit.PORT)
-        self.connection = pika.BlockingConnection(self.conn_params)
-        self.queue = queue.lower()
+class RabbitService:
+    def __init__(self, connection: AbstractRobustConnection):
+        self.connection = connection
 
-    def _open(self):
-        return self.connection.channel()
-
-    def _close(self):
-        self.connection.close()
-
-
-class Worker(RabbitBase):
-    def callback(self, ch, method, properties, body):
-        data = json.loads(body)
-        try:
-            print(data)
-        except:
-            self._open().stop_consuming()
-            self.start()
-        ch.basic_ack(delivery_tag=method.delivery_tag)
-
-    def start(self):
-        print('start')
-        channel = self._open()
-        channel.queue_declare(queue=self.queue, durable=True)
-        channel.basic_consume(self.queue, self.callback)
-        channel.start_consuming()
-
-
-if __name__ == '__main__':
-    Worker(settings.rabbit.QUEUE_1).start()
+    async def consume(self, queue: str):
+        async with self.connection.channel() as channel:
+            queue = await channel.declare_queue(
+                name=queue,
+                durable=True,
+            )
+            async with queue.iterator() as iterator:
+                async for data in iterator:
+                    async with data.process():
+                        return json.loads(data.body)

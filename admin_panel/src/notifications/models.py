25afa656e109a46e2422008b45ab9581717e7451
Sergey Shuktomov <sshuktomov@gmail.com>
2023-01-11 01:08:13 +0300

    Исправил модели

--- admin_panel/src/notifications/models.py
+++ admin_panel/src/notifications/models.py
@@ -7,24 +7,40 @@ class ContentType(models.TextChoices):
     NEW_FILM = 'new_film'
     NEW_USER = 'new_user'
     REVIEW_LIKE = 'review_like'
-    EXTRA_MAIL = 'extra'
+    CUSTOM_MAIL = 'custom'
 
 
 class Notification(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    notification_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    content_id = models.UUIDField(db_index=True)
     content_type = models.CharField(choices=ContentType.choices, max_length=15)
-    content = models.JSONField()
     last_update = models.DateTimeField(auto_now=True)
     last_notification_send = models.DateTimeField(blank=True, null=True)
 
     class Meta:
         db_table = 'notification'
+        constraints = [
+            models.UniqueConstraint(fields=['content_id', 'content_type'], name='idx_notification'),
+        ]
+
+
+class Content(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    content_id = models.UUIDField(db_index=True)
+    content_type = models.CharField(choices=ContentType.choices, max_length=15)
+    content = models.JSONField()
+
+    class Meta:
+        db_table = 'content'
+        constraints = [
+            models.UniqueConstraint(fields=['content_id', 'content_type'], name='idx_content'),
+        ]
 
 
 class Subscription(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
-    notification = models.ForeignKey('Notification', on_delete=models.CASCADE)
+    user_id = models.UUIDField(db_index=True)
+    notification_id = models.UUIDField(db_index=True)
     last_notification_send_to_user = models.DateTimeField(blank=True, null=True)
 
     class Meta:
@@ -39,6 +55,7 @@ class CommunicationType(models.TextChoices):
 
 class User(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_id = models.UUIDField(db_index=True)
     communication_method = models.CharField(choices=CommunicationType.choices, max_length=15)
     allow_communication = models.BooleanField()
 

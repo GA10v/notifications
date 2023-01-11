68cfcd809c4e118b3eff5fd9d3a8c9aee69641f6
Sergey Shuktomov <sshuktomov@gmail.com>
2023-01-12 00:59:12 +0300

    Добавил в api настройки posgres

--- app/src/core/config.py
+++ app/src/core/config.py
@@ -12,7 +12,7 @@ class BaseConfig(BaseSettings):
 class FastapiSettings(BaseConfig):
     HOST: str = 'localhost'
     PORT: int = 8080
-    NOTIFIC_PREFIX: str = '/app/v1/notification'
+    NOTIFIC_PREFIX: str = '/api/v1/notification'
 
     class Config:
         env_prefix = 'FASTAPI_'
@@ -36,6 +36,21 @@ class RabbitMQSettings(BaseConfig):
         env_prefix = 'RABBIT_'
 
 
+class PostgresSettings(BaseConfig):
+    DB: str = 'db'
+    USER: str = 'guest'
+    PASSWORD: str = 'guest'
+    HOST: str = 'localhost'
+    PORT: int = 5432
+
+    @property
+    def uri(self):
+        return f'postgresql+asyncpg://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DB}'
+
+    class Config:
+        env_prefix = 'POSTGRES_'
+
+
 class JWTSettings(BaseConfig):
     SECRET_KEY: str = '245585dbb5cbe2f151742298d61d364880575bff0bdcbf4ae383f0180e7e47dd'
     ALGORITHM: str = 'HS256'
@@ -55,6 +70,7 @@ class ProjectSettings(BaseConfig):
     PROJECT_NAME: str = 'Notification'
     BASE_DIR = Path(__file__).parent.parent
     rabbit: RabbitMQSettings = RabbitMQSettings()
+    postgres: PostgresSettings = PostgresSettings()
     fastapi: FastapiSettings = FastapiSettings()
     jwt: JWTSettings = JWTSettings()
     auth: AUTHSettings = AUTHSettings()

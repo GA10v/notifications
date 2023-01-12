d94c89565e01cfbeb7b63d0550edf10f7bfbd0d8
Sergey Shuktomov <sshuktomov@gmail.com>
2023-01-12 18:44:26 +0300

    Добавил тесты по review и welcome

---
+++ app/tests/functional/config.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+
+from pydantic import BaseSettings
+
+
+class BaseConfig(BaseSettings):
+    class Config:
+        env_file = Path(Path(__file__).parent.parent.parent.parent, '.env')
+        env_file_encoding = 'utf-8'
+
+
+class JWTSettings(BaseConfig):
+    SECRET_KEY: str = '245585dbb5cbe2f151742298d61d364880575bff0bdcbf4ae383f0180e7e47dd'
+    ALGORITHM: str = 'HS256'
+
+    class Config:
+        env_prefix = 'TEST_JWT_'
+
+
+class FastapiSettings(BaseConfig):
+    HOST: str = 'localhost'
+    PORT: int = 8000
+    REVIEW_ENDPOINT: str = 'api/v1/notification/review'
+    WELCOME_ENDPOINT: str = 'api/v1/notification/welcome'
+    NEW_EPISODE_ENDPOINT: str = 'api/v1/notification/new_episode'
+    GROUP_MESSAGE_ENDPOINT: str = 'api/v1/notification/group_message'
+
+    @property
+    def service_url(self):
+        return f'http://{self.HOST}:{self.PORT}'
+
+    class Config:
+        env_prefix = 'TEST_FASTAPI_'
+
+
+class TestDataSettings(BaseConfig):
+    USER: str = '6c162475-c7ed-4461-9184-001ef3d9f264'
+    EMAIL: str = 'user@mail.ru'
+    LOGIN: str = 'user'
+    CONTENT: str = '265d6929-4c6c-4680-9ae5-6c479de6e2ee'
+
+
+class TestSettings(BaseConfig):
+    PROJECT_NAME: str = 'Notification'
+    BASE_DIR = Path(__file__).parent.parent
+    jwt: JWTSettings = JWTSettings()
+    fastapi: FastapiSettings = FastapiSettings()
+    data: TestDataSettings = TestDataSettings()
+
+
+settings = TestSettings()
---
+++ app/tests/functional/conftest.py
@@ -0,0 +1,28 @@
+import asyncio
+import uuid
+from random import choice, randint
+
+import aiohttp
+import jwt
+import pytest_asyncio
+from config import settings
+
+
+@pytest_asyncio.fixture(scope='session')
+async def session():
+    session = aiohttp.ClientSession()
+    yield session
+    await session.close()
+
+
+@pytest_asyncio.fixture(scope='session')
+def event_loop():
+    loop = asyncio.get_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest_asyncio.fixture(scope='session')
+def access_token():
+    data = {'sub': settings.data.USER, 'permissions': [], 'is_super': True}
+    return jwt.encode(data, settings.jwt.SECRET_KEY, settings.jwt.ALGORITHM)

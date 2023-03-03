42a40d660d16c678831b3b152b109ead9e928878
Захаров Павел <pzakharov83@gmail.com>
2023-03-03 15:28:39 +0300

    add models

---
+++ api/v1/admin_panel/notifications/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from .mixins import TimeStampedMixin, UUIDMixin
+
+TEXT_FIELD_LEN = 255
+
+
+class Template(UUIDMixin, TimeStampedMixin):  # type: ignore[misc]
+    title = models.CharField(_('title'), max_length=TEXT_FIELD_LEN, unique=True)
+    template_content = models.TextField(_('template_content'), blank=True)

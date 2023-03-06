55dcafda5e7a756d59d545db746b7d37109a81d6
Galovach Andrey <a.galovach@yandex.ru>
2023-03-06 03:32:00 +0300

    добавил producer для enricher

---
+++ services/enricher/src/broker/producer.py
@@ -0,0 +1,73 @@
+import json
+from abc import ABC, abstractmethod
+from typing import Any
+
+from aio_pika import DeliveryMode, ExchangeType, Message
+from broker.rabbit import RabbitMQBroker
+from core.config import settings
+
+
+class ProducerProtocol(ABC):
+    @abstractmethod
+    async def publish(self, msg: dict[str, Any], **kwargs) -> None:
+        ...
+
+
+class RabbitMQProducer(ProducerProtocol, RabbitMQBroker):
+    def __init__(
+        self,
+        incoming_queue: str = settings.rabbit.QUEUE_TO_SEND.lower(),
+        retry_queue: str = settings.rabbit.QUEUE_RETRY_SEND.lower(),
+        incoming_exchange: str = settings.rabbit.EXCHENGE_INCOMING_2.lower(),
+        retry_exchange: str = settings.rabbit.EXCHENGE_RETRY_2.lower(),
+        **kwargs,
+    ) -> None:
+        super().__init__(**kwargs)
+        self.incoming_queue = incoming_queue
+        self.retry_queue = retry_queue
+        self.incoming_exchange = incoming_exchange
+        self.retry_exchange = retry_exchange
+
+    async def publish(self, msg: dict[str, Any], **kwargs) -> None:
+        if not self.channel_pool:
+            await self.get_channel_pool()
+
+        async with self.channel_pool.acquire() as channel:
+            incoming_exchange = await channel.declare_exchange(
+                name=self.incoming_exchange,
+                type=ExchangeType.DIRECT,
+                durable=True,
+            )
+            retry_exchange = await channel.declare_exchange(
+                name=self.retry_exchange,
+                type=ExchangeType.DIRECT,
+                durable=True,
+            )
+            incoming_queue = await channel.declare_queue(
+                name=self.incoming_queue,
+                durable=True,
+                arguments={
+                    'x-dead-letter-exchange': self.incoming_exchange,
+                },
+            )
+            retry_queue = await channel.declare_queue(
+                name=self.retry_queue,
+                durable=True,
+                arguments={
+                    'x-dead-letter-exchange': self.incoming_exchange,
+                    'x-message-ttl': settings.rabbit.MESSAGE_TTL_MS,
+                },
+            )
+
+            await incoming_queue.bind(incoming_exchange)
+            await retry_queue.bind(retry_exchange)
+
+            message = Message(
+                body=json.dumps(msg).encode('utf-8'),
+                delivery_mode=DeliveryMode.PERSISTENT,
+            )
+
+            await incoming_exchange.publish(
+                message=message,
+                routing_key=self.incoming_queue,
+            )

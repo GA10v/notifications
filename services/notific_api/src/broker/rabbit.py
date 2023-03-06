d7135956e60e95941541c6b69572e96aa7d16e49
Galovach Andrey <a.galovach@yandex.ru>
2023-03-06 03:18:27 +0300

    добавил producer для notic_api

---
+++ services/notific_api/src/broker/rabbit.py
@@ -0,0 +1,91 @@
+import json
+from abc import ABC, abstractmethod
+
+import aio_pika
+from aio_pika import DeliveryMode, ExchangeType, Message
+from core.config import settings
+
+
+class ProducerProtocol(ABC):
+    @abstractmethod
+    async def init_producer(
+        self,
+        uri: str,
+        incoming_queue: str,
+        retry_queue: str,
+        incoming_exchange: str,
+        retry_exchange: str,
+        **kwargs,
+    ) -> None:
+        ...
+
+    @abstractmethod
+    async def send_msg(self, msg: dict, **kwargs) -> None:
+        ...
+
+
+class RabbitMQProducer(ProducerProtocol):
+    def __init__(self) -> None:
+        self.connection = None
+        self.channel = None
+        self.incoming_queue = None
+        self.retry_queue = None
+        self.incoming_exchange = None
+        self.retry_exchange = None
+
+    async def init_producer(
+        self,
+        uri: str,
+        incoming_queue: str,
+        retry_queue: str,
+        incoming_exchange: str,
+        retry_exchange: str,
+        **kwargs,
+    ) -> None:
+        self.connection = await aio_pika.connect_robust(uri)
+        self.channel = await self.connection.channel()
+        self.incoming_queue = await self.channel.declare_queue(
+            name=incoming_queue,
+            durable=True,
+            arguments={
+                'x-dead-letter-exchange': settings.rabbit.EXCHENGE_INCOMING_1.lower(),
+            },
+        )
+        self.retry_queue = await self.channel.declare_queue(
+            name=retry_queue,
+            durable=True,
+            arguments={
+                'x-dead-letter-exchange': settings.rabbit.EXCHENGE_INCOMING_1.lower(),
+                'x-message-ttl': settings.rabbit.MESSAGE_TTL_MS,
+            },
+        )
+        self.incoming_exchange = await self.channel.declare_exchange(
+            name=incoming_exchange,
+            type=ExchangeType.DIRECT,
+            durable=True,
+        )
+        self.retry_exchange = await self.channel.declare_exchange(
+            name=retry_exchange,
+            type=ExchangeType.DIRECT,
+            durable=True,
+        )
+
+        await self.incoming_queue.bind(self.incoming_exchange)
+        await self.retry_queue.bind(self.retry_exchange)
+
+    async def send_msg(self, msg: dict, **kwargs) -> None:
+        message = Message(
+            body=json.dumps(msg).encode('utf-8'),
+            delivery_mode=DeliveryMode.PERSISTENT,
+        )
+        await self.incoming_exchange.publish(
+            message=message,
+            routing_key=self.incoming_queue.name,
+        )
+
+
+producer: RabbitMQProducer = RabbitMQProducer()
+
+
+async def get_producer() -> RabbitMQProducer:
+    return producer
---
+++ services/notific_api/src/core/config.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+
+from pydantic import BaseSettings
+
+
+class BaseConfig(BaseSettings):
+    class Config:
+        env_file = Path(Path(__file__).parent.parent.parent.parent.parent, 'env')
+        env_file_encoding = 'utf-8'
+
+
+class FastapiSetting(BaseConfig):
+    HOST: str = 'localhost'
+    PORT: int = 8080
+    NOTIFIC_PREFIX: str = '/app/v1/notification'
+
+    class Config:
+        env_prefix = 'FASTAPI_'
+
+
+class RabbitMQSetting(BaseConfig):
+    USER: str = 'guest'
+    PASSWORD: str = 'guest'
+    HOST: str = 'localhost'
+    PORT: int = 5672
+    EXCHENGE_INCOMING_1: str = 'Exchange_incoming_1'
+    EXCHENGE_INCOMING_2: str = 'Exchange_incoming_2'
+    EXCHENGE_RETRY_1: str = 'Exchange_retry_1'
+    EXCHENGE_RETRY_2: str = 'Exchange_retry_2'
+    QUEUE_TO_ENRICH: str = 'Queue_to_enrich'
+    QUEUE_TO_SEND: str = 'Queue_to_send'
+    QUEUE_RETRY_ENRICH: str = 'Queue_retry_to_enrich'
+    QUEUE_RETRY_SEND: str = 'Queue_retry_to_send'
+    MESSAGE_TTL_MS: int = 10000
+
+    @property
+    def uri(self):
+        return f'amqp://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}'
+
+    class Config:
+        env_prefix = 'RABBIT_'
+
+
+class ProjectSettings(BaseConfig):
+    PROJECT_NAME: str = 'Notification_api'
+    BASE_DIR = Path(__file__).parent.parent
+    fastapi: FastapiSetting = FastapiSetting()
+    rabbit: RabbitMQSetting = RabbitMQSetting()
+
+
+settings = ProjectSettings()

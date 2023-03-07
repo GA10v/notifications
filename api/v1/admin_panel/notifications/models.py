c33ec8aea56961f4a0c8badc2048ebf322428797
Захаров Павел <pzakharov83@gmail.com>
2023-03-07 12:54:07 +0300

    add template files

--- api/v1/admin_panel/notifications/models.py
+++ api/v1/admin_panel/notifications/models.py
@@ -9,6 +9,7 @@ TEXT_FIELD_LEN = 255
 class Template(UUIDMixin, TimeStampedMixin):  # type: ignore[misc]
     title = models.CharField(_('title'), max_length=TEXT_FIELD_LEN, unique=True)
     template_content = models.TextField(_('template_content'), blank=True)
+    template_files = models.FilePathField(path='templates/', allow_folders=True, allow_files=False)
 
     class Meta:
         verbose_name = _('Template')

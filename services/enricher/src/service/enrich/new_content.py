f58abd75ec1b6a1b66a2e59b9e1136c1ae10cb84
Galovach Andrey <a.galovach@yandex.ru>
2023-03-09 08:28:09 +0300

    добавил парсеры для событий

---
+++ services/enricher/src/service/enrich/new_content.py
@@ -0,0 +1,43 @@
+import aiohttp
+from aiohttp.client_exceptions import ClientError
+from core.config import settings
+from models.events import Event
+from models.payloads import NewContentContext
+from service.enrich.protocol import PayloadsProtocol
+from utils.auth import _headers
+
+
+class NewContentPayloads(PayloadsProtocol):
+    def __init__(self, data: Event) -> None:
+        self.data = data
+        self.auth_endpoint = f'{settings.auth.uri}user_info/'
+        self.admin_panel_endpoint = f'{settings.admin_panel.uri}movie/'
+        self._headers = _headers()
+
+    async def payload(self) -> NewContentContext:
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.post(
+                    f'{self.auth_endpoint}{self.data.context.user_id}',
+                    headers=self._headers,
+                ) as resp:
+                    _user = await resp.json()
+
+                async with session.post(
+                    f'{self.admin_panel_endpoint}{self.data.context.movie_id}',
+                    headers=self._headers,
+                ) as resp:
+                    _movie = await resp.json()
+        except ClientError as ex:  # noqa: F841
+            print('все хуйня, давай по новой!!!')  # noqa: T201
+            return None
+
+        payload = NewContentContext(
+            user_name=_user.get('name'),
+            email=_user.get('email'),
+            phone_number=_user.get('phone_number'),
+            telegram_name=_user.get('telegram_name'),
+            delivery_type=_user.get('delivery_type'),
+            movie_title=_movie.get('title'),
+        )
+        print('payload: ', payload)  # noqa: T201

0fb4836157f5e887540b7044c3c94a979484a535
Galovach Andrey <a.galovach@yandex.ru>
2023-03-09 10:02:47 +0300

    добавил handler для событий

---
+++ services/enricher/src/service/enrich/handler.py
@@ -0,0 +1,18 @@
+from models.base import EventType
+from models.events import Event
+from models.payloads import payload
+from service.enrich.new_content import NewContentPayloads
+from service.enrich.new_user import NewUserPayload
+
+
+async def get_payload(data: Event) -> payload:
+    if data.event_type == EventType.welcome:
+        payload = NewUserPayload(data)
+    elif data.event_type == EventType.new_content:
+        payload = NewContentPayloads(data)
+    # elif data.event_type == EventType.new_likes:  # noqa: E800
+    #     ...
+    # elif data.event_type == EventType.promo:  # noqa: E800
+    #     ...
+
+    return await payload.payload()

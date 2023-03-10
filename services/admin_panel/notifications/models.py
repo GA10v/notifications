e7bcfb1c0a5448f078c7cf51b8a3eaf2464d6920
Захаров Павел <pzakharov83@gmail.com>
2023-03-10 10:59:24 +0300

    add fields for sms and push

--- services/admin_panel/notifications/models.py
+++ services/admin_panel/notifications/models.py
@@ -8,7 +8,8 @@ TEXT_FIELD_LEN = 255
 
 class Template(UUIDMixin, TimeStampedMixin):  # type: ignore[misc]
     title = models.CharField(_('title'), max_length=TEXT_FIELD_LEN, unique=True)
-    template_content = models.TextField(_('template_content'), blank=True)
+    template_content = models.TextField(_('template_content'), blank=False)
+    template_short_description = models.TextField(_('template_short'), blank=False)
     template_files = models.FilePathField(path='templates/', allow_folders=True, allow_files=False)
 
     class Meta:

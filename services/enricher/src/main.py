0e928d050f1062b907df4232cfc056eefc473be8
Galovach Andrey <a.galovach@yandex.ru>
2023-03-13 04:19:46 +0300

    переписал handler

--- services/enricher/src/main.py
+++ services/enricher/src/main.py
@@ -3,16 +3,24 @@ from typing import Any
 
 from broker.consumer import RabbitMQConsumer
 from broker.producer import RabbitMQProducer
+from db.storage import PGStorage
+from models.events import Event
 from service.builder import BuilderService
+from service.enrich.handler import get_payload
+from service.template import get_template
 
 consumer = RabbitMQConsumer()
 producer = RabbitMQProducer()
 enricher = BuilderService()
+db = PGStorage()
 
 
 async def handler(message: dict[str, Any]) -> None:
-    print('handler message: ', message)  # noqa: T201
-    new_message = await enricher.build(message)
+    event = Event(**message)
+    async with db as conn:
+        _template = await get_template(db=conn, data=event)
+    payload = await get_payload(data=event)
+    new_message = await enricher.build(data=payload, template=_template)
     print('handler new_message: ', new_message)  # noqa: T201
     await producer.publish(msg=new_message)
 

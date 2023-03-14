726094519cf598a733a0c9082e2e88463cdde46a
Захаров Павел <pzakharov83@gmail.com>
2023-03-14 11:23:50 +0300

    add translations

--- services/admin_panel/notifications/models.py
+++ services/admin_panel/notifications/models.py
@@ -6,21 +6,6 @@ from .mixins import TimeStampedMixin, UUIDMixin
 TEXT_FIELD_LEN = 255
 
 
-# class Template(UUIDMixin, TimeStampedMixin):  # type: ignore[misc]  # noqa: E800
-#     title = models.CharField(_('title'), max_length=TEXT_FIELD_LEN, unique=True)  # noqa: E800
-#     template_content = models.TextField(_('template_content'), blank=False)  # noqa: E800
-#     template_short_description = models.TextField(_('template_short'), blank=False)  # noqa: E800
-#     template_files = models.FilePathField(path='templates/', allow_folders=True, allow_files=False)  # noqa: E800
-
-#     class Meta:  # noqa: E800
-#         verbose_name = _('Template')  # noqa: E800
-#         verbose_name_plural = _('Templates')  # noqa: E800
-#         ordering = ['title']  # noqa: E800
-
-#     def __str__(self):  # noqa: E800
-#         return self.title[:15]  # noqa: E800
-
-
 class Template(UUIDMixin, TimeStampedMixin):  # type: ignore[misc]
     class EventType(models.TextChoices):
         WELCOME = 'welcome_message'
@@ -36,14 +21,27 @@ class Template(UUIDMixin, TimeStampedMixin):  # type: ignore[misc]
         GROUP_4 = 'group_4'
 
     title = models.CharField(_('title'), max_length=TEXT_FIELD_LEN, unique=True)
-    event_type = models.CharField(max_length=TEXT_FIELD_LEN, choices=EventType.choices, default=EventType.NEW_CONTENT)
-    subject = models.TextField(_('subject'), max_length=TEXT_FIELD_LEN, default='')
-    template_files = models.FilePathField(path='templates/', allow_folders=True, allow_files=False)
-    text_to_promo = models.TextField(max_length=4096, blank=True, default='')
-    text_msg = models.TextField(max_length=4096, blank=True, default='')
-    user_group = models.CharField(max_length=TEXT_FIELD_LEN, choices=UserGroup.choices, default=UserGroup.ALL_USER)
+    event_type = models.CharField(
+        _('event_type'),
+        max_length=TEXT_FIELD_LEN,
+        choices=EventType.choices,
+        default=EventType.NEW_CONTENT,
+    )
+    subject = models.CharField(_('subject'), max_length=TEXT_FIELD_LEN, default='')
+    template_files = models.FilePathField(_('template_files'), path='templates/', allow_folders=True, allow_files=False)
+    text_to_promo = models.TextField(_('text_to_promo'), max_length=4096, blank=True, default='')
+    text_msg = models.TextField(_('text_msg'), max_length=4096, blank=True, default='')
+    user_group = models.CharField(
+        _('user group'),
+        max_length=TEXT_FIELD_LEN,
+        choices=UserGroup.choices,
+        default=UserGroup.ALL_USER,
+    )
 
     class Meta:
         verbose_name = _('Template')
         verbose_name_plural = _('Templates')
         ordering = ['title']
+
+    def __str__(self):
+        return f'{self.title:15} - {self.event_type:15} - {self.subject:15}'

fa607a315e7224149c6cc0ed8a322e1783965032
Захаров Павел <pzakharov83@gmail.com>
2023-03-14 11:36:09 +0300

    check connection

--- services/admin_panel/notifications/tasks.py
+++ services/admin_panel/notifications/tasks.py
@@ -1,10 +1,14 @@
 import logging
 
 from celery import shared_task  # type: ignore[attr-defined]
+from services.generator.src.service.main import ProcessTask
 
 logger = logging.getLogger(__name__)
 
 
 @shared_task(name='hello')
 def hello():
+    task = None
     logger.debug('Hello there!')
+    new_task = ProcessTask()
+    new_task.perform_task(task)

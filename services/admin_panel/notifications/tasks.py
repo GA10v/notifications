f129b82c96ea78ef7d4728e83e35f60180c697e2
Захаров Павел <pzakharov83@gmail.com>
2023-03-14 12:53:47 +0300

    sample

--- services/admin_panel/notifications/tasks.py
+++ services/admin_panel/notifications/tasks.py
@@ -1,14 +1,13 @@
 import logging
 
 from celery import shared_task  # type: ignore[attr-defined]
-from services.generator.src.service.main import ProcessTask
+from services.generator.src.service.main import EventType, ProcessTask, Task
 
 logger = logging.getLogger(__name__)
 
 
-@shared_task(name='hello')
-def hello():
-    task = None
-    logger.debug('Hello there!')
-    new_task = ProcessTask()
-    new_task.perform_task(task)
+@shared_task(name='create_new_content_task')
+def create_new_content_task(*args, **kwargs):
+    task = Task(event_type=EventType.new_content, context=kwargs)
+    generator = ProcessTask()
+    generator.perform_task(task)

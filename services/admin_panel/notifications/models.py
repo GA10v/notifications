1ca2ad55023bbb4eefb0fba2128187003689e77a
Galovach Andrey <a.galovach@yandex.ru>
2023-03-14 15:44:18 +0300

    потрогал admin_panel

--- services/admin_panel/notifications/models.py
+++ services/admin_panel/notifications/models.py
@@ -4,30 +4,35 @@ from django.utils.translation import gettext_lazy as _
 from .mixins import TimeStampedMixin, UUIDMixin
 
 TEXT_FIELD_LEN = 255
+MSG_FIELD_LEN = 4096
+PROMO_FIELD_LEN = 4096
 
 
-# class Template(UUIDMixin, TimeStampedMixin):  # type: ignore[misc]  # noqa: E800
-#     title = models.CharField(_('title'), max_length=TEXT_FIELD_LEN, unique=True)  # noqa: E800
-#     template_content = models.TextField(_('template_content'), blank=False)  # noqa: E800
-#     template_short_description = models.TextField(_('template_short'), blank=False)  # noqa: E800
-#     template_files = models.FilePathField(path='templates/', allow_folders=True, allow_files=False)  # noqa: E800
-
-#     class Meta:  # noqa: E800
-#         verbose_name = _('Template')  # noqa: E800
-#         verbose_name_plural = _('Templates')  # noqa: E800
-#         ordering = ['title']  # noqa: E800
-
-#     def __str__(self):  # noqa: E800
-#         return self.title[:15]  # noqa: E800
+class EventType(models.TextChoices):
+    WELCOME = 'welcome_message'
+    NEW_CONTENT = 'new_content'
+    NEW_LIKES = 'new_likes'
+    PROMO = 'promo'
 
 
 class Template(UUIDMixin, TimeStampedMixin):  # type: ignore[misc]
-    class EventType(models.TextChoices):
-        WELCOME = 'welcome_message'
-        NEW_CONTENT = 'new_content'
-        NEW_LIKES = 'new_likes'
-        PROMO = 'promo'
 
+    title = models.CharField(_('title'), max_length=TEXT_FIELD_LEN, unique=True)
+    event_type = models.CharField(max_length=TEXT_FIELD_LEN, choices=EventType.choices, default=EventType.NEW_CONTENT)
+    subject = models.TextField(_('subject'), max_length=TEXT_FIELD_LEN, default='')
+    template_files = models.FilePathField(path='templates/', allow_folders=True, allow_files=False)
+    text_msg = models.TextField(max_length=MSG_FIELD_LEN, blank=True, default='')
+
+    def __str__(self):
+        return f'{self.event_type}:<{self.pkid}>'
+
+    class Meta:
+        verbose_name = _('Template')
+        verbose_name_plural = _('Templates')
+        ordering = ['title']
+
+
+class Task(UUIDMixin, TimeStampedMixin):  # type: ignore[misc]
     class UserGroup(models.TextChoices):
         ALL_USER = 'all'
         GROUP_1 = 'group_1'
@@ -36,14 +41,14 @@ class Template(UUIDMixin, TimeStampedMixin):  # type: ignore[misc]
         GROUP_4 = 'group_4'
 
     title = models.CharField(_('title'), max_length=TEXT_FIELD_LEN, unique=True)
+    template_pkid = models.ForeignKey(Template, on_delete=models.CASCADE)
     event_type = models.CharField(max_length=TEXT_FIELD_LEN, choices=EventType.choices, default=EventType.NEW_CONTENT)
-    subject = models.TextField(_('subject'), max_length=TEXT_FIELD_LEN, default='')
-    template_files = models.FilePathField(path='templates/', allow_folders=True, allow_files=False)
-    text_to_promo = models.TextField(max_length=4096, blank=True, default='')
-    text_msg = models.TextField(max_length=4096, blank=True, default='')
     user_group = models.CharField(max_length=TEXT_FIELD_LEN, choices=UserGroup.choices, default=UserGroup.ALL_USER)
+    movie_id = models.CharField(max_length=TEXT_FIELD_LEN, blank=True, default='')
+    text_to_promo = models.CharField(max_length=PROMO_FIELD_LEN, blank=True, default='')
+
+    def __str__(self):
+        return self.title
 
     class Meta:
-        verbose_name = _('Template')
-        verbose_name_plural = _('Templates')
         ordering = ['title']

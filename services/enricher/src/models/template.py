e7ed320df5d36a5a17d688538ea9b7b3a63f8cc2
Galovach Andrey <a.galovach@yandex.ru>
2023-03-16 15:22:40 +0300

    потрогал enricher

--- services/enricher/src/models/template.py
+++ services/enricher/src/models/template.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from pydantic import BaseModel
 
 from models.base import DeliveryType
@@ -7,6 +9,15 @@ class TemplateFromDB(BaseModel):
     subject: str
     template_files: str
     text_msg: str
+
+
+class ReviewsFromDB(BaseModel):
+    pkid: str
+    movie_id: str
+    author_id: str
+    review_id: str
+    likes_count: int
+    modified: datetime
 
 
 class TemplateToSender(BaseModel):

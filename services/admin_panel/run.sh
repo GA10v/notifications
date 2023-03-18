df7ad9e70b758214f91294cf32503bcea960f2d7
Захаров Павел <pzakharov83@gmail.com>
2023-03-18 19:34:55 +0300

    remove extra actions django app

--- services/admin_panel/run.sh
+++ services/admin_panel/run.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py createsuperuser --noinput \
-                                --username $DJANGO_SUPERUSER_USERNAME \
-                                --email $DJANGO_SUPERUSER_EMAIL
+python3 manage.py createsuperuser --noinput
 python3 manage.py collectstatic --noinput
 python3 manage.py compilemessages
 uwsgi --strict --ini uwsgi/uwsgi.ini

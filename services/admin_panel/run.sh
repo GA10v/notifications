c26546d46eb0648e3057a73c825ebdfc07aa11cd
Захаров Павел <pzakharov83@gmail.com>
2023-03-20 15:28:39 +0300

    cleanup

--- services/admin_panel/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-python3 manage.py migrate
-python3 manage.py createsuperuser --noinput
-python3 manage.py collectstatic --noinput
-python3 manage.py compilemessages
-uwsgi --strict --ini uwsgi/uwsgi.ini
--- services/admin_panel/shell_scripts/entrypoint_admin_panel.sh
+++ services/admin_panel/shell_scripts/entrypoint_admin_panel.sh
@@ -4,4 +4,3 @@ python3 manage.py createsuperuser --noinput
 python3 manage.py collectstatic --noinput
 python3 manage.py compilemessages
 uwsgi --strict --ini uwsgi/uwsgi.ini
-cd src && gunicorn --bind 0.0.0.0:8080 -w 4 -k uvicorn.workers.UvicornH11Worker main:app
